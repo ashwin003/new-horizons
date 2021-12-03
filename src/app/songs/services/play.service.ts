01ef7738c8c2bbc007756ba223fdc88dc1935316
Ashwin <ashwin031192@gmail.com>
2021-12-03 21:40:20 +0530

    Actually play/pause music

--- src/app/songs/services/play.service.ts
+++ src/app/songs/services/play.service.ts
@@ -5,11 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class PlayService {
   playing = false;
+  player = new Audio();
   currentlyPlaying: string | null = null;
-  constructor() {}
+  constructor() {
+    const instance = this;
+    this.player.addEventListener('ended', function () {
+      instance.currentlyPlaying = null;
+      instance.playing = false;
+    });
+  }
 
   play(uri: string) {
     this.currentlyPlaying = uri;
+    this.player.src = uri;
+    this.player.play();
     this.playing = true;
   }
 
@@ -25,6 +34,7 @@ export class PlayService {
   }
 
   pause() {
+    this.player.pause();
     this.playing = false;
   }
 
