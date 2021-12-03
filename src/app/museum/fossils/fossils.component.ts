4674da3a4db837549c93c40afc772808d4c399c5
Ashwin <ashwin031192@gmail.com>
2021-12-03 23:32:12 +0530

    Minor UI fixes

--- src/app/museum/fossils/fossils.component.ts
+++ src/app/museum/fossils/fossils.component.ts
@@ -11,6 +11,7 @@ import { FossilModalComponent } from '../fossil-modal/fossil-modal.component';
 })
 export class FossilsComponent implements OnInit {
   fossils: any[] = [];
+
   constructor(
     private fossilService: FossilService,
     private translationService: TranslationService,
@@ -18,7 +19,28 @@ export class FossilsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.fossilService.getAll().subscribe((response) => (this.fossils = response));
+    this.fossilService.getAll().subscribe((response) => (this.fossils = this.sortData(response)));
+  }
+
+  sortData(data: any[]) {
+    return data.sort(this.sorter);
+  }
+
+  sorter(a: any, b: any): any {
+    const sortOrder = ['skull', 'chest', 'torso', 'pelvis', 'tail'];
+    const aPart = a['part-of'],
+      bPart = b['part-of'];
+
+    const bodyPartIndex = (name: any) => {
+      const splitted = name.split('_');
+      return sortOrder.indexOf(splitted[splitted.length - 1]);
+    };
+    if (aPart === bPart) {
+      const aIndex = bodyPartIndex(a.fileName),
+        bIndex = bodyPartIndex(b.fileName);
+      return aIndex - bIndex;
+    }
+    return aPart - bPart;
   }
 
   getValue(dictionary: Map<string, string>, prefix: string) {
