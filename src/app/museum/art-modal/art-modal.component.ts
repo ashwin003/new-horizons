292b4d72e70324e4169d46aeb4f826499122075b
Ashwin <ashwin031192@gmail.com>
2021-12-04 14:39:17 +0530

    Save submitted art and fossils

--- src/app/museum/art-modal/art-modal.component.ts
+++ src/app/museum/art-modal/art-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { SavedEntityService } from '@app/@shared/services/saved-entity.service';
 import { TranslationService } from '@app/@shared/services/translation.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-art-modal',
@@ -8,9 +10,27 @@ import { TranslationService } from '@app/@shared/services/translation.service';
   styleUrls: ['./art-modal.component.scss'],
 })
 export class ArtModalComponent implements OnInit {
-  constructor(private translationService: TranslationService, @Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(
+    private translationService: TranslationService,
+    private savedEntityService: SavedEntityService,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<ArtModalComponent>
+  ) {}
+
+  icon$ = this.savedEntityService.isSaved(this.data.fileName).pipe<string>(map((e) => (e ? 'star' : 'star_border')));
 
   ngOnInit(): void {}
+
+  toggleSaveState() {
+    this.savedEntityService.isSaved(this.data.fileName).subscribe((d) => {
+      if (d) {
+        this.savedEntityService.remove(this.data.fileName);
+      } else {
+        this.savedEntityService.save(this.data.fileName, 'Art');
+      }
+      this.dialogRef.close();
+    });
+  }
 
   getValue(dictionary: Map<string, string>, prefix: string) {
     return this.translationService.getValue(dictionary, prefix);
--- src/app/museum/fossil-modal/fossil-modal.component.ts
+++ src/app/museum/fossil-modal/fossil-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { SavedEntityService } from '@app/@shared/services/saved-entity.service';
 import { TranslationService } from '@app/@shared/services/translation.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-fossil-modal',
@@ -8,9 +10,27 @@ import { TranslationService } from '@app/@shared/services/translation.service';
   styleUrls: ['./fossil-modal.component.scss'],
 })
 export class FossilModalComponent implements OnInit {
-  constructor(private translationService: TranslationService, @Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(
+    private translationService: TranslationService,
+    private savedEntityService: SavedEntityService,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<FossilModalComponent>
+  ) {}
+
+  icon$ = this.savedEntityService.isSaved(this.data.fileName).pipe<string>(map((e) => (e ? 'star' : 'star_border')));
 
   ngOnInit(): void {}
+
+  toggleSaveState() {
+    this.savedEntityService.isSaved(this.data.fileName).subscribe((d) => {
+      if (d) {
+        this.savedEntityService.remove(this.data.fileName);
+      } else {
+        this.savedEntityService.save(this.data.fileName, 'Fossil');
+      }
+      this.dialogRef.close();
+    });
+  }
 
   getValue(dictionary: Map<string, string>, prefix: string) {
     return this.translationService.getValue(dictionary, prefix);
